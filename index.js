9bd7d3a8b74a4f1a9c6d8c8240668282eb8a4898
Franklin Kong <franklinkong981@gmail.com>
2023-09-02 15:55:49 -0700

    Delete and negative buttons

--- index.js
+++ index.js
@@ -15,9 +15,18 @@ function handleCalcButtonClick(buttonContent) {
         statementToParse = "";
         statementToDisplay = "";
         screenText.textContent = "0";
-    } else if (Number.isInteger(parseInt(buttonContent))) {
+    } else if (buttonContent === "Del" && statementToDisplay !== "") {
+        statementToParse = statementToParse.slice(0, -1);
+        statementToDisplay = statementToDisplay.slice(0, -1);
+        screenText.textContent = (statementToDisplay === "") ? "0" : statementToDisplay;
+    } else if (Number.isInteger(parseInt(buttonContent)) || buttonContent === ".") {
         statementToParse += buttonContent;
         statementToDisplay += buttonContent;
+        screenText.textContent = statementToDisplay;
+    }
+    else if (buttonContent === "Neg") {
+        statementToParse += "-";
+        statementToDisplay += "-";
         screenText.textContent = statementToDisplay;
     }
 }
