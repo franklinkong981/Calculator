ef8d50c2aa3f224fc86ee08beea614b12ae0b19f
Franklin Kong <franklinkong981@gmail.com>
2023-09-04 10:40:20 -0700

    Added event handler for operations

--- index.js
+++ index.js
@@ -3,23 +3,49 @@ let statementToParse = ""; //Expression that we'll evaluate.
 let statementToDisplay = ""; //What we'll display on the calculator.
 let previousAnswer = "";
 
-const calcButtons = document.querySelectorAll('button');
-calcButtons.forEach((currentCalcButton) => 
+const calcNonOperationButtons = document.querySelectorAll('button');
+calcNonOperationButtons.forEach((currentCalcButton) => 
     currentCalcButton.addEventListener('click', function() {
-        handleCalcButtonClick(currentCalcButton.textContent);
-    }
-));
+        handleNonOperationClick(currentCalcButton.textContent);
+    })
+);
+const calcOperationButtons = document.querySelectorAll('.operation-button');
+calcOperationButtons.forEach((currentCalcButton) =>
+    currentCalcButton.addEventListener('click', function() {
+        handleNonOperationClick(currentCalcButton.id, currentCalcButton.textContent);
+    })
+);
 
-function handleCalcButtonClick(buttonContent) {
+function handleNonOperationClick(buttonContent) {
     if (buttonContent === "Clear") {
         handleClearButton();
     } else if (buttonContent === "Del" && statementToDisplay !== "") {
         handleDeleteButton();
+    } else if (buttonContent === "Ans") {
+        handleAnswerButton();
     } else if (Number.isInteger(parseInt(buttonContent)) || buttonContent === ".") {
         handleNumberButton(buttonContent);
     }
     else if (buttonContent === "Neg") {
         handleNegativeButton();
+    }
+}
+
+function handleOperationClick(buttonId, buttonContent) {
+    if (buttonId === "addition") {
+        handleAddition(buttonContent);
+    }
+    else if (buttonId === "subtraction") {
+        handleSubtraction(buttonContent);
+    }
+    else if (buttonId === "multiplication") {
+        handleMultiplication(buttonContent);
+    }
+    else if (buttonId === "division") {
+        handleDivision(buttonContent);
+    }
+    else if (buttonId === "equals") {
+        handleEnter();
     }
 }
 
@@ -47,6 +73,10 @@ function handleNegativeButton() {
     statementToParse += "-";
     statementToDisplay += "-";
     screenText.textContent = statementToDisplay;
+}
+
+function handleAnswerButton() {
+
 }
 
 
